06e66fefad0a4793831c7cad0cc4104da3243e4e
Pranav-Bobde <bobdep31@gmail.com>
2021-09-20 22:00:25 +0530

    Setup a basic Express JS Server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("Hello Community Classroom");
+//import the Express JS Framework
+const express = require("express");
+//Initialising an app instance.
+const app = express();
+//Configure a port address.
+const port = 5000;
+app.listen(port, () => {
+    console.log('Server started in port ${port}.');
+});
