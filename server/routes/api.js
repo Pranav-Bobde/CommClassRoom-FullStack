9821b4c05d6e6965100a9be99b961ad1e8ad02c3
Pranav-Bobde <bobdep31@gmail.com>
2021-09-22 03:03:08 +0530

    Created a skeleton structure for auth endpoint.

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,11 +2,14 @@
 const express = require("express");
 //Create a Router App
 const app = express.Router();
-
+//Import the routes
+const auth = require("./api/auth")
 //Create a Get Request handler for /
 app.get("/", (req, res) => {
     res.json("Welcome to Community Classroom API!");
 })
+//API routes handler
+app.use("/auth", auth)
 
 //Export the Router
 module.exports = app;
---
+++ server/routes/api/auth.js
@@ -0,0 +1,8 @@
+//Import Exprees JS Library
+const express = require("express");
+//Create a Router App
+const app = express.Router();
+
+
+//Export the Router
+module.exports = app;
