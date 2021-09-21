fb46b85984e17b7c4d7b3163db185a57c03309a8
Pranav-Bobde <bobdep31@gmail.com>
2021-09-22 03:31:22 +0530

    Moved Users to its own constants.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -4,31 +4,6 @@ const express = require("express");
 const app = express.Router();
 
 //Temporary User's Data
-const Users = {
-    Pranav: {
-        Name: "Praveen Kumar Purushothaman",
-        Password: "Praveen",
-        Type: "Admin", //Student, Mentor, Admin, Disabled
-        Verified: true,
-
-        Personal: {
-            Email: "praveen@gmail.com",
-            Phone: "+447894561230"
-        },
-        CreatedAt: new Date()
-    },
-    PrPraveen: {
-        Name: "Pranav Bobde",
-        Password: "Pranav",
-        Type: "Student", //Student, Mentor, Admin, Disabled
-        Verified: false,
-        Personal: {
-            Email: "pranav@gmail.com",
-            Phone: "+919876543210"
-        },
-        CreatedAt: new Date()
-    }
-
-};
+const Users = require("../../constants/Users.json")
 //Export the Router
 module.exports = app;
