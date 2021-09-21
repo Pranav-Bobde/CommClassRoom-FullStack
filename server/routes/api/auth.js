a01e1999aa77020d9bfd0fc4d4fd45c64b341b82
Pranav-Bobde <bobdep31@gmail.com>
2021-09-22 03:48:31 +0530

    Created endpoint to check CurrentUser.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -3,5 +3,18 @@ const express = require("express");
 //Create a Router App
 const app = express.Router();
 
+let CurrentUser = null;
+
+//Setup Login Path
+app.get("/login", (req, res) => {
+    if(CurrentUser) {
+        res.json(CurrentUser);
+    } else {
+        res.status(400).json({
+            Error: "Not Logged In."
+        })
+    }
+});
+
 //Export the Router
 module.exports = app;
